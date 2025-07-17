28310ddfc691bb3bce3708316ec96b06b26d5eda
신현준 <sshj1215@gmail.com>
2025-07-17 17:50:31 +0900

    [feat] 디스코드 로그인

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import express , { Request, Response } from "express";
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { AppDataSource } from './data-source';
+import 'reflect-metadata';
 import oauthRouter from './router/oauthRouter';
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log('DB 연결 성공');
+
+        app.get('/', (req: Request, res: Response) => {
+            res.send('<h1>Hello World!</h1>')
+        });
+
+        app.use('/auth', oauthRouter);
+
+        app.listen(PORT, () => {
+            console.log(`Example app listening on port ${PORT}`)
+        });
+    }).catch((err) => {
+    console.error('DB 연결 실패:',err);
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Hello World!</h1>')
