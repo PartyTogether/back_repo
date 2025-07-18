26181028feb5e5be4113307208baadfd2b3e4e89
신현준 <sshj1215@gmail.com>
2025-07-18 14:22:28 +0900

    [fix] 라우íialize ã안에 ë깨ãrouter 넣기

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import oauthRouter from './router/oauthRouter';
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+// 실행시 DB 연결 및 라우터 설정
 AppDataSource.initialize()
     .then(() => {
         console.log('DB 연결 성공');
@@ -24,9 +25,3 @@ AppDataSource.initialize()
     }).catch((err) => {
     console.error('DB 연결 실패:',err);
 });
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello World!</h1>')
-});
-
-app.use('/auth', oauthRouter);
